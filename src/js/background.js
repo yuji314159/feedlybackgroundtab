bb2788458a952077caeec37cbb324ba405794348
yuji314159 <yuji314159@gmail.com>
2025-03-07 00:21:43 +0900

    インデントをスペース4つに統一した

--- src/js/background.js
+++ src/js/background.js
@@ -12,30 +12,30 @@
  * This will open up a new non-focused tab with the url it was sent
  */
 chrome.extension.onMessage.addListener(
-	function(messageObject) {
-		chrome.tabs.create({
-			url: messageObject.url,
-			active: false
-		});
-	}
+    function(messageObject) {
+        chrome.tabs.create({
+            url: messageObject.url,
+            active: false
+        });
+    }
 );
 
 /**
  * Notify users of updates/installed versions
  */
 chrome.runtime.onInstalled.addListener(function(details) {
-	if (details.reason == 'update') {
-		var url = '';
-		switch (details.previousVersion) {
-			case "0.1":
-				// big update: added options and customized key
-				url = 'data:text/html,<h1>FeedlyBackgroundTab Updated!</h1><p>Feedly Background Tab has been updated.  Please check the <a href="https://chrome.google.com/webstore/detail/feedly-background-tab/gjlijkhcebalcchkhgaiflaooghmoegk">extension page</a> for more information.</p>';
-				break;
-		}
-		if (url) {
-			chrome.tabs.create({
-	            url: url
-	        });
-		}
-	}
+    if (details.reason == 'update') {
+        var url = '';
+        switch (details.previousVersion) {
+            case "0.1":
+                // big update: added options and customized key
+                url = 'data:text/html,<h1>FeedlyBackgroundTab Updated!</h1><p>Feedly Background Tab has been updated.  Please check the <a href="https://chrome.google.com/webstore/detail/feedly-background-tab/gjlijkhcebalcchkhgaiflaooghmoegk">extension page</a> for more information.</p>';
+                break;
+        }
+        if (url) {
+            chrome.tabs.create({
+                url: url
+            });
+        }
+    }
 });
--- src/js/keypress.js
+++ src/js/keypress.js
@@ -4,51 +4,51 @@
  * @author Aaron Saray (http://aaronsaray.com)
  */
 (function(){
-	/**
-	 * The selectors used to find the URL
-	 * @type {array}
-	 */
+    /**
+     * The selectors used to find the URL
+     * @type {array}
+     */
     var selectors = [
-		'div.selectedEntry a.title',			// title bar for active entry, collapsed or expanded
-		'.selectedEntry a.visitWebsiteButton',	// the button square button on list view
-		'.list-entries .selected a.visitWebsiteButton',	// the button square button on list view
-		'a.visitWebsiteButton',					// the floating one for card view
-		'.entry.selected a.title'				// title bar for active entry in React-based collapsed list view
+        'div.selectedEntry a.title',                    // title bar for active entry, collapsed or expanded
+        '.selectedEntry a.visitWebsiteButton',          // the button square button on list view
+        '.list-entries .selected a.visitWebsiteButton', // the button square button on list view
+        'a.visitWebsiteButton',                         // the floating one for card view
+        '.entry.selected a.title'                       // title bar for active entry in React-based collapsed list view
     ];
-	
-	/**
-	 * Main feedlybackgroundtab constructor
-	 */
-	var FBT = function() {
+    
+    /**
+     * Main feedlybackgroundtab constructor
+     */
+    var FBT = function() {
 
-		/**
-		 * The default key code which is ;
-		 * @type {number}
-		 * @private
-		 */
-		var _triggerKeyCode = 59;
+        /**
+         * The default key code which is ;
+         * @type {number}
+         * @private
+         */
+        var _triggerKeyCode = 59;
 
-		/**
-		 * Used to create the default key code from local storage
-		 * Also modifies the help popup
-		 */
-		this.init = function() {
-			chrome.storage.sync.get('shortcutKey', function(settings) {
-				if (settings.shortcutKey) {
-					_triggerKeyCode = settings.shortcutKey.charCodeAt(0);
-				}
-			});
-		};
+        /**
+         * Used to create the default key code from local storage
+         * Also modifies the help popup
+         */
+        this.init = function() {
+            chrome.storage.sync.get('shortcutKey', function(settings) {
+                if (settings.shortcutKey) {
+                    _triggerKeyCode = settings.shortcutKey.charCodeAt(0);
+                }
+            });
+        };
 
-		/**
-		 * handler for key press - must be not in textarea or input and must be not altered
-		 * Then it sends extension request
-		 * @param e
-		 */
-		this.keyPressHandler = function(e) {
-			var tag = e.target.tagName.toLowerCase();
-			if (tag != 'input' && tag != 'textarea') {
-				if ((!e.altKey && !e.ctrlKey) && e.keyCode == _triggerKeyCode) {
+        /**
+         * handler for key press - must be not in textarea or input and must be not altered
+         * Then it sends extension request
+         * @param e
+         */
+        this.keyPressHandler = function(e) {
+            var tag = e.target.tagName.toLowerCase();
+            if (tag != 'input' && tag != 'textarea') {
+                if ((!e.altKey && !e.ctrlKey) && e.keyCode == _triggerKeyCode) {
                     var url;
                     for (var x in selectors) {
                         url = document.querySelector(selectors[x]);
@@ -56,20 +56,20 @@
                             break;
                         }
                     }
-					if (url) {
-						chrome.extension.sendMessage({url: url.href});
-					}
+                    if (url) {
+                        chrome.extension.sendMessage({url: url.href});
+                    }
                     else {
                         console.log("Could not find any selectors from: " + selectors.join());
                     }
-				}
-			}
-		}
-	};
+                }
+            }
+        }
+    };
 
-	if (window == top) {
-		var fbt = new FBT();
-		fbt.init();
-		window.addEventListener('keypress', fbt.keyPressHandler, false);
-	}
+    if (window == top) {
+        var fbt = new FBT();
+        fbt.init();
+        window.addEventListener('keypress', fbt.keyPressHandler, false);
+    }
 })();
